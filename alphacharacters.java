d3c5ccf42fd5a9a559a2457ba12f58af4b41debe
aliwert <154356044+aliwert@users.noreply.github.com>
2024-05-08 01:37:05 +0300

    alpha characters

---
+++ alphacharacters.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class alphacharacters {
+    public static void main(String[] args) {
+        //first solution
+        String str = "heKL75jki8#";
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLetter(ch)) {
+                count++;
+            }
+        }
+        System.out.println(str + " icinde  " + count + " has alpha characters");
+
+        //second solution
+        System.out.println(str.replaceAll("[^a-zA-Z]", "").length());
+    }
+}
